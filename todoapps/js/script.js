cda78487d308377ec1f3e1412165b78dbb038c19
WijiKurniawan <wijikurniawan121@gmail.com>
2022-07-25 01:42:12 +0700

    perbaiki bug

--- todoapps/js/script.js
+++ todoapps/js/script.js
@@ -61,17 +61,125 @@ function makeTodo(todoObject) {
     container.classList.add('item', 'shadow');
     container.append(textContainer);
     container.setAttribute('id', `todo-${todoObject.id}`);
+
+    if (todoObject.isCompleted) {
+        const undoButton = document.createElement('button');
+        undoButton.classList.add('undo-button');
+     
+        undoButton.addEventListener('click', function () {
+          undoTaskFromCompleted(todoObject.id);
+        });
+     
+        const trashButton = document.createElement('button');
+        trashButton.classList.add('trash-button');
+     
+        trashButton.addEventListener('click', function () {
+          removeTaskFromCompleted(todoObject.id);
+        });
+     
+        container.append(undoButton, trashButton);
+      } else {
+        const checkButton = document.createElement('button');
+        checkButton.classList.add('check-button');
+        
+        checkButton.addEventListener('click', function () {
+          addTaskToCompleted(todoObject.id);
+        });
+        
+        container.append(checkButton);
+      }
    
-    return container; 
+    return container;
   }
-  //panggil tampil halaman web
+
+//render Event
   document.addEventListener(RENDER_EVENT, function () {
-    console.log(todos);
     const uncompletedTODOList = document.getElementById('todos');
     uncompletedTODOList.innerHTML = '';
    
+    const completedTODOList = document.getElementById('completed-todos');
+    completedTODOList.innerHTML = '';
+
     for (const todoItem of todos) {
-      const todoElement = makeTodo(todoItem);
-      uncompletedTODOList.append(todoElement);
+        const todoElement = makeTodo(todoItem);
+        if (!todoItem.isCompleted)
+          uncompletedTODOList.append(todoElement);
+        else
+          completedTODOList.append(todoElement);
+      }
+    });
+// render event
+
+// function ceklis Todo ke yang sudah dilakukan start 
+  function addTaskToCompleted (todoId) {
+    const todoTarget = findTodo(todoId);
+   
+    if (todoTarget == null) return;
+   
+    todoTarget.isCompleted = true;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  function findTodo(todoId) {
+    for (const todoItem of todos) {
+      if (todoItem.id === todoId) {
+        return todoItem;
+      }
     }
-  });
+    return null;
+  }
+// end
+
+//function untuk hapus dan undo Todo start
+function removeTaskFromCompleted(todoId) {
+    const todoTarget = findTodoIndex(todoId);
+   
+    if (todoTarget === -1) return;
+   
+    todos.splice(todoTarget, 1);
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+   
+   
+  function undoTaskFromCompleted(todoId) {
+    const todoTarget = findTodo(todoId);
+   
+    if (todoTarget == null) return;
+   
+    todoTarget.isCompleted = false;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+// end
+
+// function untuk Todo hapus dan undo
+function removeTaskFromCompleted(todoId) {
+    const todoTarget = findTodoIndex(todoId);
+   
+    if (todoTarget === -1) return;
+   
+    todos.splice(todoTarget, 1);
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+   
+   
+  function undoTaskFromCompleted(todoId) {
+    const todoTarget = findTodo(todoId);
+   
+    if (todoTarget == null) return;
+   
+    todoTarget.isCompleted = false;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+// end
+
+// fix error fungsi find todoIndex tidak ditemukan
+function findTodoIndex(todoId) {
+    for (const index in todos) {
+      if (todos[index].id === todoId) {
+        return index;
+      }
+    }
+   
+    return -1;
+  }
+  // selesai buat function findTodoIndex
